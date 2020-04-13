8b1e82cd4fb2078cc346123cec6cd1551ba62c1f
philldeveloper <philldeveloper@gmail.com>
2020-04-13 16:47:56 -0300

    1304

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.user')
 
+<link rel="stylesheet" type="text/css" href="{{asset('css/alerts.css')}}">
+
 @section('header')
 
 <!-- <div class="page-header pb-10 page-header-dark bg-gradient-primary-to-secondary" style="background: url(https://pcbx.us/bexy.jpg);"> -->
 <div class="page-header pb-10 page-header-dark bg-dark mb-3" style="background: url(https://pcbx.us/bexy.jpg);">
     <div class="container-fluid">
         <div class="page-header-content row">
-            <div class="col-lg-4">
+            <div class="col-lg-4 m-3">
                 <h1 class="page-header-title">
                     <span>Bem vindo, <b class="text-uppercase">{{Auth::user()->nome}}</b></span>
                 </h1>
 
                 <div class="page-header-subtitle">
                     <div class="badge badge-warning font-weight-bold text-dark">Afiliado gold</div>
-                <div class="badge badge-info font-weight-bold text-dark">Nivel: {{Auth::user()->nivel->id}}</div>
+                    <div class="badge badge-info font-weight-bold text-dark">Nivel: {{Auth::user()->nivel->id}}</div>
                     @if(Auth::user()->status == "pendente" || Auth::user()->status == "inativo")
                     <div class="badge badge-danger font-weight-bold text-light">{{Auth::user()->status}}</div>
                     @else
@@ -21,13 +23,21 @@
                     @endif
                 </div>
             </div>
-            <div class="col-lg-3 row justify-content-right ml-auto">
-                <h2 class="mr-3 text-light">Saldo Disponível:</h2>
-                <div class="page-header-subtitle">
+
+            <div class="col-lg-3 row justify-content-right ml-auto m-3">
+
+                <div class="col-sm-12">
+                    <div class="alert fade alert-simple alert-success alert-dismissible text-left font__family-montserrat font__size-16 font__weight-light brk-library-rendered rendered show">
+                        <i class="start-icon far fa-check-circle faa-tada animated"></i>
+                        <strong class="font-weight-bold">Saldo Disponível:</strong> <b>R$ {{Auth::user()->saldo}}</b>.
+                    </div>
+                </div>
+
+                <!-- <div class="page-header-subtitle">
                     <span class=" badge badge-success">
                         <h4 class="font-weight-bold text-light">R$ {{Auth::user()->saldo}}</h4>
                     </span>
-                </div>
+                </div> -->
             </div>
         </div>
         @if(Auth::user()->status == "pendente")
