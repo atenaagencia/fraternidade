ba0cd70f66a9762a0e20c2fcbea22b8335e5d2c9
herbetjr <herbetjr@gmail.com>
2020-04-14 01:17:43 -0300

    goijnjlk.

--- resources/views/user/features/modal_termo_user.blade.php
+++ resources/views/user/features/modal_termo_user.blade.php
@@ -3,16 +3,46 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title text-dark" id="termo_user_modalLabel">Termo de Lorem Ipsum</h5>
+        <h5 class="modal-title text-dark" id="termo_user_modalLabel">Termo de compromisso</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body text-dark">
-        <h2 class="py-4 font-weight-bold">Termos de Lorem Ipsum</h2><hr>
-        <p class="lead">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos facilis et quaerat, ab beatae repudiandae, earum, alias deleniti saepe veniam explicabo vitae cum at sit error libero animi id maxime!
-        </p>
+      <div class="modal-body text-dark text-justify" style="height: 300px; overflow-y:scroll;">
+        <h2 class="py-4 font-weight-bold">Win Happy</h2><hr>
+    <blockquote>
+      <p>1 - N&atilde;o existe nenhum tipo de investimento. O membro DOADOR faz as suas DOA&Ccedil;&Otilde;ES de livre e
+        espont&acirc;nea vontade, sem coa&ccedil;&atilde;o ou influ&ecirc;ncia de quem quer que seja, gratuitamente e sem
+        condi&ccedil;&otilde;es ou encargos de qualquer natureza, do valor referente a cada etapa do sistema.</p>
+      <p>2 - N&atilde;o existe nenhuma promessa ou garantia de retorno financeiro.</p>
+      <p>3 - Para participar do sistema Win Happy o usu&aacute;rio deve ser maior de 18 anos.</p>
+      <p>4 - Ao preencher seus dados no sistema, informe os dados reais para facilitar a comunica&ccedil;&atilde;o. Todas as
+        doa&ccedil;&otilde;es ser&atilde;o feitas atrav&eacute;s da Conta Banc&aacute;ria e este ser&aacute; os meios que
+        vamos trabalhar para agilizar o sistema.</p>
+      <p>5 - Ap&oacute;s realizar o pagamento e fazer os procedimentos de confirma&ccedil;&atilde;o atrav&eacute;s do seu
+        backoffice, entre em contato com o Donat&aacute;rio e informe sobre o pagamento, solicitando a
+        confirma&ccedil;&atilde;o de recebimento e a libera&ccedil;&atilde;o imediata do seu cadastro
+        (ativa&ccedil;&atilde;o).</p>
+      <p>6 - Os PAGAMENTOS s&atilde;o realizados diretamente na Conta Banc&aacute;ria de cada participante!</p>
+      <p>7 - Todo participante tem o direito de deixar de participar do sistema a qualquer momento.</p>
+      <p>8 - O usu&aacute;rio ter&aacute; um prazo de ativa&ccedil;&atilde;o para todos os Niveis e planos;</p>
+      <p>9 - N&atilde;o tem atravessadores e nenhum lucro direto para a empresa X ou Y.</p>
+      <p>10 - A velocidade de ciclagens depende exclusivamente do ritmo de desenvolvimento da equipe de cada membro;</p>
+      <p>11 - O uso de documentos pr&oacute;prios ou de terceiros &eacute; de inteira responsabilidade do membro cadastrado;
+      </p>
+      <p>12 - Os anexos de comprovantes de pagamentos s&oacute; ser&atilde;o aceitos nas seguintes extens&otilde;es: jpg,
+        png e pdf;</p>
+      <p>13 - Voc&ecirc; paga apenas um valor inicial no plano em que se cadastrar uma &uacute;nica vez, os demais valores
+        do plano em que se cadastrar s&atilde;o oriundos do pr&oacute;prio projeto.</p>
+      <p>A - No Ciclo 1, o usu&aacute;rio ter&aacute; at&eacute; &agrave;s 12:00hrs do pr&oacute;ximo dia &uacute;til para
+        ter sua conta ativada pelo donat&aacute;rio;</p>
+      <p>B - Nos demais Niveis, o usu&aacute;rio ter&aacute; at&eacute; &agrave;s 24hrs do pr&oacute;ximo dia &uacute;til
+        para ter sua matriz ativada pelo donat&aacute;rio.</p>
+      <p>Se voc&ecirc; concorda com esses termos, fa&ccedil;a seu cadastro no sistema e siga as instru&ccedil;&otilde;es do
+        mesmo. Caso n&atilde;o concorde com os termos, interrompa seu cadastro e deixe a p&aacute;gina.</p>
+      <p>Qualquer d&uacute;vidas, cr&iacute;ticas, propostas, sugest&otilde;es, problemas t&eacute;cnicos ou outros. entrar
+        em contato atrav&eacute;s de um dos e-mails abaixo:</p>
+    </blockquote>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
