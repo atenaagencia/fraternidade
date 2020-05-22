3cd0112e67f85f39bd9192cfc7dc1d1fd7d35437
herbetjr <herbetjr@gmail.com>
2020-05-22 15:34:53 -0300

    dsds

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'nome','usuario','email','telefone','whatsapp','picpay','saldo','status','perfil','nivel_id','password',
+        'nome','usuario','email','telefone','whatsapp','picpay','saldo','status','perfil','nivel_id', 'cont_deposito','password',
     ];
 
     /**
