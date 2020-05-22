69ba4552e66a46ef45922deef48c5a39d5bcfe30
herbetjr <herbetjr@gmail.com>
2020-05-22 18:27:26 -0300

    ddsjhdgaj

--- resources/views/user/features/transacao/r02.blade.php
+++ resources/views/user/features/transacao/r02.blade.php
@@ -1,6 +1,6 @@
 @inject('transacao', 'App\Transacao')
 @php
-$r02_dep = $transacao->where('origem_id', 2)->where('destinatario_id',Auth::user()->id)->where('status','<>','liberado')->get();
+$r02_dep = $transacao->where('origem_id', 2)->where('destinatario_id',Auth::user()->id)->get();
 
     $espera = count($r02_dep);
     @endphp
