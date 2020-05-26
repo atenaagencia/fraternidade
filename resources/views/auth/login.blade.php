699b1ff08adc92896a9248204c3684019601e8a0
herbetjr <herbetjr@gmail.com>
2020-05-26 16:48:02 -0300

    herbe t

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,20 +3,23 @@
 <link href="{{ asset('css/login-page.css') }}" rel="stylesheet">
 
 <style>
-.form-check {
-    position: relative !important;
-    display: block !important;
-    padding-left: 1.25rem !important;
-}
-input[type=checkbox], input[type=radio] {
-    box-sizing: border-box !important;
-    padding: 0 !important;
-}
-.form-check-input {
-    position: absolute !important;
-    margin-top: .3rem !important;
-    margin-left: -1.25rem !important;
-}
+    .form-check {
+        position: relative !important;
+        display: block !important;
+        padding-left: 1.25rem !important;
+    }
+
+    input[type=checkbox],
+    input[type=radio] {
+        box-sizing: border-box !important;
+        padding: 0 !important;
+    }
+
+    .form-check-input {
+        position: absolute !important;
+        margin-top: .3rem !important;
+        margin-left: -1.25rem !important;
+    }
 </style>
 @section('content')
 <br><br>
@@ -43,28 +46,35 @@ input[type=checkbox], input[type=radio] {
         <form class="register-form" method="POST" action="{{ route('register') }}">
             @csrf
 
-            <p class="lead font-weight-bold text-left mb-2 mt-3 text-dark">INFORMAÇÕES PESSOAIS</p><hr>
-            <input id="name" type="text" class="@error('name') is-invalid @enderror font-weight-bold text-dark lead" name="nome"
-                value="{{ old('nome') }}" required autocomplete="nome" autofocus placeholder="Nome">
+            <p class="lead font-weight-bold text-left mb-2 mt-3 text-dark">INFORMAÇÕES PESSOAIS</p>
+            <hr>
+            <input id="name" type="text" class="@error('name') is-invalid @enderror font-weight-bold text-dark lead"
+                name="nome" value="{{ old('nome') }}" required autocomplete="nome" autofocus placeholder="Nome">
             @error('name')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
             @enderror
             <small class="text-dark text-left">Sem espaçamento, somente letras minusculas</small>
-            <input id="usuario" type="text" class="@error('usuario') is-invalid @enderror font-weight-bold text-dark lead" name="usuario"
+            <input id="usuario" type="text"
+                class="@error('usuario') is-invalid @enderror font-weight-bold text-dark lead" name="usuario"
                 value="{{ old('nome') }}" required autocomplete="nome" autofocus placeholder="Usuário">
-               
-            <input id="email" type="email" class="@error('email') is-invalid @enderror font-weight-bold text-dark lead" name="email"
-                value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
+            @error('usuario')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+
+            <input id="email" type="email" class="@error('email') is-invalid @enderror font-weight-bold text-dark lead"
+                name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
             @error('email')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
             @enderror
 
-            <input id="email" type="text" class="font-weight-bold text-dark lead" name="telefone" value="{{ old('telefone') }}" required
-                autocomplete="telefone" autofocus placeholder="Telefone">
+            <input id="email" type="text" class="font-weight-bold text-dark lead" name="telefone"
+                value="{{ old('telefone') }}" required autocomplete="telefone" autofocus placeholder="Telefone">
 
             @error('telefone')
             <span class="invalid-feedback" role="alert">
@@ -72,25 +82,28 @@ input[type=checkbox], input[type=radio] {
             </span>
             @enderror
 
-            <input id="email" type="text" class="font-weight-bold text-dark lead" name="whatsapp" value="{{ old('whatsapp') }}" required
-                autocomplete="whatsapp" autofocus placeholder="Whatsapp">
+            <input id="email" type="text" class="font-weight-bold text-dark lead" name="whatsapp"
+                value="{{ old('whatsapp') }}" required autocomplete="whatsapp" autofocus placeholder="Whatsapp">
 
             @error('whatsapp')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
             @enderror
-            <input id="email" type="text" class="font-weight-bold text-dark lead" name="picpay" value="{{ old('picpay') }}"
-                required autocomplete="picpay" autofocus placeholder="Conta PicPay" required>
-                <p class="text-dark"> Se você não tem cadastrado PicPay <a href="http://www.picpay.com/convite?@XAFO" target="_blank"> Clique Aqui</a></p>
-            
+            <input id="email" type="text" class="font-weight-bold text-dark lead" name="picpay"
+                value="{{ old('picpay') }}" required autocomplete="picpay" autofocus placeholder="Conta PicPay"
+                required>
+            <p class="text-dark"> Se você não tem cadastrado PicPay <a href="http://www.picpay.com/convite?@XAFO"
+                    target="_blank"> Clique Aqui</a></p>
+
             @error('picpay')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
             @enderror
 
-            <p class="lead font-weight-bold text-left mb-2 mt-3 text-dark">CREDENCIAIS DE ACESSO</p><hr>
+            <p class="lead font-weight-bold text-left mb-2 mt-3 text-dark">CREDENCIAIS DE ACESSO</p>
+            <hr>
 
             <input id="password" type="password" class="@error('password') is-invalid @enderror" name="password"
                 value="{{ old('password') }}" required autocomplete="password" autofocus placeholder="Senha">
@@ -109,9 +122,11 @@ input[type=checkbox], input[type=radio] {
             @enderror
 
             <div class="form-check text-dark text-left">
-                <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" style="width: 15 !important" required="">
+                <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" style="width: 15 !important"
+                    required="">
                 <label class="form-check-label lead" for="defaultCheck1">
-                    Eu aceito os Termos de Usuário. <a href="#" data-toggle="modal" data-target="#termo_user_modal">Ver aqui</a>
+                    Eu aceito os Termos de Usuário. <a href="#" data-toggle="modal" data-target="#termo_user_modal">Ver
+                        aqui</a>
                 </label>
             </div>
 
@@ -123,11 +138,12 @@ input[type=checkbox], input[type=radio] {
 
         <form lass="login-form" method="POST" action="{{ route('login') }}">
             @csrf
-            <p class="lead font-weight-bold text-left mb-2 mt-3 text-dark">INSIRA SEU USUARIO E SENHA</p><hr>
+            <p class="lead font-weight-bold text-left mb-2 mt-3 text-dark">INSIRA SEU USUARIO E SENHA</p>
+            <hr>
             <input id="email" type="text" class="@error('usuario') is-invalid @enderror" name="usuario"
                 value="{{ old('usuario') }}" required autocomplete="usuario" autofocus placeholder="Usuário.">
 
-            @error('email')
+            @error('usuario')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
@@ -176,6 +192,14 @@ input[type=checkbox], input[type=radio] {
     <div class="clearfix"></div>
 </footer> -->
 
+@if ($errors->any())
+<script>
+    $(document).ready(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+</script>
+
+@endif
 
 <script>
     $('.message a').click(function(){
