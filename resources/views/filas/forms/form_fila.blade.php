7d79a6299c0a2b51986b4a119237473c3d024728
herbetjr <herbetjr@gmail.com>
2020-05-27 19:44:25 -0300

    dsd

--- resources/views/filas/forms/form_fila.blade.php
+++ resources/views/filas/forms/form_fila.blade.php
@@ -1,11 +1,18 @@
 @inject('filiados', 'App\User')
 @php
- $filiado = $filiados->where('nivel_id',$nivel)->pluck('nome', 'id')->all();   
+ $filiado = $filiados->where('nivel_id',$nivel)->orderBy('nome','asc')->get();   
 @endphp
 
 
 {!! Form::open(['route'=>'filas.store','class'=>'user']) !!}
 {!! Form::label('', 'Usuário', []) !!}
+<select name="user_id" id="" class=" form-control" required>
+    <option value="">-----</option>
+    @foreach ($filiado as $user)
+<option value="{{$user->id}}">{{$user->nome | $user->usuario}}</option>
+
+    @endforeach
+</select>
 {!! Form::select('user_id',$filiado,'' , ['class'=>'form-control', 'required']) !!}
 {!! Form::submit('Enviar', ['class'=>['btn', 'btn-success','btn-block','mt-2']]) !!}
 {!! Form::close() !!}
