0b4aa06002da82ad34ab234c621aa34c27f09991
herbetjr <herbetjr@gmail.com>
2020-05-27 23:44:02 -0300

    xzxzx

---
+++ resources/views/erros/500.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.win-happy')
+
+<link href="{{ asset('css/login-page.css') }}" rel="stylesheet">
+
+<style>
+    .form-check {
+        position: relative !important;
+        display: block !important;
+        padding-left: 1.25rem !important;
+    }
+
+    input[type=checkbox],
+    input[type=radio] {
+        box-sizing: border-box !important;
+        padding: 0 !important;
+    }
+
+    .form-check-input {
+        position: absolute !important;
+        margin-top: .3rem !important;
+        margin-left: -1.25rem !important;
+    }
+</style>
+@section('content')
+<br><br>
+
+<div class="font-montserrat text-center mt-5 py-4 mx-auto">
+    <h1><i class="fas fa-handshake text-light mr-4 py-4"></i>Winhappy</h1><br>
+    <h1 class="display-4">Acesso ao Sistema</h1>
+</div>
+
+
+
+
+<div class="login-page">
+ 
+</div>
+
+
+
+@endsection
