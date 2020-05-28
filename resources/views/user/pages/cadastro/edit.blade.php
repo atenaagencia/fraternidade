904aea40a94dfc35ff62ffefdf7ec4736f5b769f
philldeveloper <philldeveloper@gmail.com>
2020-05-27 23:56:29 -0300

    2705

--- resources/views/user/pages/cadastro/edit.blade.php
+++ resources/views/user/pages/cadastro/edit.blade.php
@@ -1,82 +1,52 @@
 @extends('layouts.user')
 
-<style>
-    .page-header .page-header-content {
-        padding-top: 2rem !important;
-        padding-bottom: 2rem !important;
-    }
-</style>
 @section('header')
-
 <link rel="stylesheet" type="text/css" href="{{asset('css/alerts.css')}}">
 
-
-<div class="page-header page-header-dark" style="background: url(https://pcbx.us/bexy.jpg);">
+<header class="mb-5 p-4" style="background: url(https://pcbx.us/bexy.jpg);">
     <div class="container-fluid">
-        <div class="page-header-content row">
-            <div class="col-lg-4 m-3">
-                <h1 class="page-header-title">
-                    <span>Cadastro de Usuário</span>
+        <div class="row pt-2">
+            <div class="col-lg-7 col-md-6 col-sm-6 col-xs-12 mt-3 mb-3">
+                <h1 class="text-light py-1">
+                    <b class="text-uppercase font-weight-bold">Cadastro de Usuário</b>
                 </h1>
-            </div>
-
-            <div class="col-lg-3 row justify-content-right ml-auto m-3">
-
-                <div class="col-sm-12">
-                    <div class="alert fade alert-simple alert-success alert-dismissible text-left font__family-montserrat font__size-16 font__weight-light brk-library-rendered rendered show">
-                        <i class="start-icon far fa-check-circle faa-tada animated"></i>
-                        <strong class="font-weight-bold">Saldo Disponível:</strong> <b>R$ {{Auth::user()->saldo}}</b>
-                    </div>
+                <div class="page-header-subtitle">
+                    <div class="badge badge-warning font-weight-bold text-dark">Afiliado gold</div>
+                    <div class="badge badge-info font-weight-bold text-dark">Nivel: {{Auth::user()->nivel->id}}</div>
+                    @if(Auth::user()->status == "pendente" || Auth::user()->status == "inativo")
+                    <div class="badge badge-danger font-weight-bold text-light">{{Auth::user()->status}}</div>
+                    @else
+                    <div class="badge badge-success font-weight-bold text-light">{{Auth::user()->status}}</div>
+                    @endif
                 </div>
             </div>
-
-
-        </div>
-
-    </div>
-
-</div>
+            <div class="col-lg-5 col-md-6 col-sm-6 col-xs-12 mt-3 mb-3">
+                <div class="p-2 alert fade alert-simple alert-success alert-dismissible text-left font__family-montserrat font__weight-light brk-library-rendered rendered show">
+                    <i class="start-icon far fa-check-circle faa-tada animated"></i>
+                    <strong class="font-weight-bold">Saldo Disponível:</strong> <b>R$ {{number_format(Auth::user()->saldo,2,',','.')}}</b>
+                </div>
+            </div>
+        </div><!--row-->
+    </div><!--container-fluid-->
+</header>
 @endsection
 
 @section('content')
-
-
 <div class="container-fluid mt-5">
     <div class="col-12 ">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="container-fluid">
-                    <div class="card border-0 mb-4">
-                         <div class="card-header bg-user-secondary">
-                            <h1 class="text-light">Perfil de Usuário</h1>
-                        </div>
-                        <div class="card-body">
-                            <div class="mb-3">
-                            <a href="{{route('cadastro.index')}}"  class="btn btn-success"><i class="fa fa-arrow-left"></i></a>
-                            </div>
-
-                         
-                            <div>
-                               
-                               @include('user.forms.form_user')  
-                            </div>
-                            
-                           
-
-                          
-                           
-                        </div>
-                    </div>
-
+        <div class="card rounded-0 border-0 mb-4">
+                <div class="card-header rounded-0 bg-user-secondary">
+                <h1 class="text-light">Perfil de Usuário</h1>
+            </div>
+            <div class="card-body rounded-0">
+                <div class="mb-3">
+                <a href="{{route('cadastro.index')}}"  class="btn btn-success"><i class="fa fa-arrow-left"></i></a>
+                </div>
+                <div class="py-3">
+                    @include('user.forms.form_user')  
                 </div>
             </div>
-
         </div>
-        <!--col--12-->
     </div>
 </div>
-
-
-
-
 @endsection
